b8a7b9bfa7065a6cf7289991580c7ae699b29884
红色篝火 <48897680+kav1982@users.noreply.github.com>
2021-03-22 00:36:42 +0800

    Initial commit

---
+++ Assets/Custom RP/Runtime/CustomRenderPipeline.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class CameraRenderer
+{
+    CullingResults cullingResults;
+    ScriptableRenderContext context;
+    Camera camera;
+
+    const string bufferName = "Render Camera";
+    static ShaderTagId unlitShaderTagId = new ShaderTagId("SPRDefaultUnlit");
+    CommandBuffer buffer = new CommandBuffer
+    {
+        name = bufferName
+    };
+
+    public void Render (ScriptableRenderContext context, Camera camera)
+    {
+        this.context = context;
+        this.camera = camera;
+
+        if (!Cull())
+            return;
+
+        Setup();
+        DrawVisibleGeometry();
+        Submit();
+    }
+
+    void Setup()
+    {
+        context.SetupCameraProperties(camera);
+        buffer.ClearRenderTarget(true, true, Color.clear);
+        buffer.BeginSample(bufferName);      
+        ExecuteBuffer();     
+    }
+
+    void DrawVisibleGeometry()
+    {
+        var sortingSettings = new SortingSettings(camera);
+        var drawingSettings = new DrawingSettings(unlitShaderTagId, sortingSettings);
+        //指出哪些渲染队列是允许的
+        var filteringSettings = new FilteringSettings(RenderQueueRange.all);
+        
+        context.DrawRenderers(cullingResults, ref drawingSettings, ref filteringSettings);
+        context.DrawSkybox(camera);
+    }
+
+    void Submit()
+    {
+        buffer.EndSample(bufferName);
+        ExecuteBuffer();
+        context.Submit();
+    }
+
+    void ExecuteBuffer()
+    {
+        context.ExecuteCommandBuffer(buffer);
+        buffer.Clear();
+    }
+
+    bool Cull ()
+    {      
+        if (camera.TryGetCullingParameters(out ScriptableCullingParameters p))
+        {
+            cullingResults = context.Cull(ref p);
+            return true;
+        }
+        return false;
+    }
+}
+
+public class CustomRenderPipeline : RenderPipeline
+{
+    CameraRenderer renderer = new CameraRenderer();
+    protected override void Render(ScriptableRenderContext context, Camera[] cameras)
+    {
+        foreach (Camera camera in cameras)
+        {
+            renderer.Render(context, camera);
+        }
+    }
+
+}
---
+++ Assets/Custom RP/Runtime/CustomRenderPipelineAsset.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+[CreateAssetMenu(menuName = "Rendering/Custom Render Pipeline")]
+public class CustomRenderPipelineAsset : RenderPipelineAsset
+{
+    protected override RenderPipeline CreatePipeline()
+    {
+        return new CustomRenderPipeline();
+    }
+}
+
+
