db64f0cbd4636f4c939840272c15f5dc53c88563
“kav1982 <181893402@qq.com>
2021-03-22 19:41:44 +0800

    3.22 渲染流程添加

--- Assets/Custom RP/Runtime/CustomRenderPipeline.cs
+++ Assets/Custom RP/Runtime/CustomRenderPipeline.cs
@@ -3,31 +3,62 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
 
-public class CameraRenderer
+public partial class CameraRenderer
 {
     CullingResults cullingResults;
     ScriptableRenderContext context;
     Camera camera;
 
+    //static Material errorMaterial;
     const string bufferName = "Render Camera";
-    static ShaderTagId unlitShaderTagId = new ShaderTagId("SPRDefaultUnlit");
+    static ShaderTagId unlitShaderTagId = new ShaderTagId("SRPDefaultUnlit");
     CommandBuffer buffer = new CommandBuffer
     {
         name = bufferName
     };
 
+    //static ShaderTagId[] legacyShaderTagIds =
+    //{
+    //    new ShaderTagId("Always"),
+    //    new ShaderTagId("ForwardBase"),
+    //    new ShaderTagId("PrepassBase"),
+    //    new ShaderTagId("Vertex"),
+    //    new ShaderTagId("VertexLMRGBM"),
+    //    new ShaderTagId("VertexLM")      
+    //};
+
     public void Render (ScriptableRenderContext context, Camera camera)
     {
         this.context = context;
         this.camera = camera;
-
+        PrepareForSceneWindow();
         if (!Cull())
             return;
 
         Setup();
         DrawVisibleGeometry();
+        DrawUnsupportedShaders();
+        DrawGizmos();
         Submit();
     }
+
+    //对不支持的shader指定默认的渲染材质
+    //void DrawUnsupportedShaders()
+    //{
+    //    if (errorMaterial == null)
+    //        errorMaterial = new Material(Shader.Find("Hidden/InternalErrorShader"));
+    //    var drawingSettings = new DrawingSettings(legacyShaderTagIds[0], new SortingSettings(camera))
+    //    {
+    //        overrideMaterial = errorMaterial
+    //    };
+    //    //遍历legacyShaderTagIds中所有的shader类型
+    //    for (int i = 1; i<legacyShaderTagIds.Length; i++)
+    //    {
+    //        drawingSettings.SetShaderPassName(i, legacyShaderTagIds[i]);
+    //    }
+    //    var filteringSettings = FilteringSettings.defaultValue;
+    //    context.DrawRenderers(cullingResults, ref drawingSettings, ref filteringSettings);
+    //}
 
     void Setup()
     {
@@ -39,14 +70,18 @@ public class CameraRenderer
 
     void DrawVisibleGeometry()
     {
-        var sortingSettings = new SortingSettings(camera);
+        var sortingSettings = new SortingSettings(camera)
+        {
+            criteria = SortingCriteria.CommonOpaque
+        };
         var drawingSettings = new DrawingSettings(unlitShaderTagId, sortingSettings);
         //指出哪些渲染队列是允许的
-        var filteringSettings = new FilteringSettings(RenderQueueRange.all);
-        
+        var filteringSettings = new FilteringSettings(RenderQueueRange.all);       
         context.DrawRenderers(cullingResults, ref drawingSettings, ref filteringSettings);
         context.DrawSkybox(camera);
     }
+
+ 
 
     void Submit()
     {
