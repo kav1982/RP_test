7fd633c6589411ff7fc512d86663e08e4a5a5d7e
红色篝火 <48897680+kav1982@users.noreply.github.com>
2021-05-22 07:07:59 +0800

    5.22 litinput更新

--- Assets/Custom RP/ShaderLibrary/LitInput.hlsl
+++ Assets/Custom RP/ShaderLibrary/LitInput.hlsl
@@ -19,3 +19,27 @@ float2 TransformBaseUV (float2 baseUV)
 	return baseUV * baseST.xy +baseST.zw;
 }
 
+float4 GetBase (float2 baseUV)
+{
+	float4 map = SAMPLE_TEXTURE2D(_BaseMap, sampler_BaseMap, baseUV);
+	float4 color = UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _BaseColor);
+	return map * color;
+}
+
+float GetCutoff (float2 baseUV)
+{
+	return UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _Cutoff);
+}
+
+float GetMetallic (float2 baseUV)
+{
+	return UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _Metallic);
+}
+
+float GetSmoothness (float2 baseUV)
+{	
+	return UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _Smoothness);
+}
+
+#endif
+
