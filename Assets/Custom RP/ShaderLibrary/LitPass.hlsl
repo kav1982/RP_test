6e69b679686766dd368aea3960a1be56d8063efc
红色篝火 <48897680+kav1982@users.noreply.github.com>
2023-01-22 22:58:09 +0800

    LodTest-Start

--- Assets/Custom RP/ShaderLibrary/LitPass.hlsl
+++ Assets/Custom RP/ShaderLibrary/LitPass.hlsl
@@ -1,7 +1,7 @@
 ﻿#ifndef CUSTOM_LIT_PASS_INCLUDED
 #define CUSTOM_LIT_PASS_INCLUDED
 
-//#include "../ShaderLibrary/Common.hlsl"
+#include "../ShaderLibrary/Common.hlsl"
 #include "../ShaderLibrary/Surface.hlsl"
 #include "../ShaderLibrary/Shadows.hlsl"
 #include "../ShaderLibrary/Light.hlsl"
@@ -64,6 +64,9 @@ Varyings LitPassVertex(Attributes input)
 float4 LitPassFragment(Varyings input) : SV_TARGET
 {
 	UNITY_SETUP_INSTANCE_ID(input);
+	#if defined(LOD_FADE_CROSSFADE)
+		return unity_LODFade.x;
+	#endif
 //float4 baseMap = SAMPLE_TEXTURE2D(_BaseMap, sampler_BaseMap, input.baseUV);
 //float4 baseColor = UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _BaseColor);	
 //float4 base =  baseMap * baseColor;
--- Assets/Custom RP/Shaders/Lit.shader
+++ Assets/Custom RP/Shaders/Lit.shader
@@ -49,6 +49,7 @@
 			#pragma multi_compile _ _CASCADE_BLEND_SOFT _CASCADE_BLEND_DITHER
             #pragma multi_compile _ _SHADOW_MASK_ALWAYS _SHADOW_MASK_DISTANCE            
 			#pragma multi_compile _ LIGHTMAP_ON
+            #pragma multi_compile _ LOD_FADE_CROSSFADE
             #pragma multi_compile_instancing
             #pragma vertex LitPassVertex
             #pragma fragment LitPassFragment
